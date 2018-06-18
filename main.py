3d073a178b6331f5945540519bd484084bf09d4a
FilipeKN4 <filipekn4@gmail.com>
2018-06-17 21:44:46 -0300

    "Get all courses datas"
    Co-authored-by: nukdown <belchior.emanoel@gmail.com>

--- main.py
+++ main.py
@@ -2,41 +2,61 @@ from data_source_departament import DataSourceDepartament
 from data_source_subject import DataSourceSubject,DataSourceSubjectsDarcy
 from data_sink_subject import DataSinkSubject
 from data_sink_departament import DataSinkDepartament
+import requests
+from bs4 import BeautifulSoup
 
 DARCY = "DARCY"
 FUP = "FUP"
 FCE = "FCE"
 FGA = "FGA"
 
-cont = 0
-
 list_departaments = [FGA,FCE,FUP,DARCY]
 list_departaments_numbers = [4,3,2,1]
+list_campi_numbers = [4,3,2,1]
 list_departaments_cods = [650,660,638]
 
-for aux in range(len(list_departaments)):
+def run ():
 
-    departament = DataSourceDepartament.read("https://matriculaweb.unb.br/graduacao/oferta_dep.aspx?cod=" +
-                                             str(list_departaments_numbers[aux]), list_departaments[aux])
-    DataSinkDepartament.write(departament, list_departaments[aux])
+    for aux in range(len(list_departaments)):
+
+        departament = DataSourceDepartament.read("https://matriculaweb.unb.br/graduacao/oferta_dep.aspx?cod=" +
+                                                 str(list_departaments_numbers[aux]), list_departaments[aux])
+        DataSinkDepartament.write(departament, list_departaments[aux])
 
     for new_aux in range(len(list_departaments_cods)):
 
         subjects = DataSourceSubject.read("https://matriculaweb.unb.br/graduacao/oferta_dis.aspx?cod=" +
-                                          str(list_departaments_cods[new_aux]),list_departaments[new_aux])
+                                              str(list_departaments_cods[new_aux]), new_aux)
         DataSinkSubject.write(subjects)
 
-subjects_darcy = DataSourceDepartament.read("https://matriculaweb.unb.br/graduacao/oferta_dep.aspx?cod=" +
-                                            str(list_departaments_numbers[3]),list_departaments[3])
 
-subjects_darcy_cods = subjects_darcy.list_cods
-subjects_darcy_initials = subjects_darcy.list_initials
-size_subjects_darcy = 75
+    subjects_darcy = DataSourceDepartament.read("https://matriculaweb.unb.br/graduacao/oferta_dep.aspx?cod=" +
+                                                str(list_departaments_numbers[3]),list_departaments[3])
 
-for aux in range(size_subjects_darcy):
+    subjects_darcy_cods = subjects_darcy.list_cods
+    subjects_darcy_initials = subjects_darcy.list_initials
+    size_subjects_darcy = 76
 
-    subject = DataSourceSubjectsDarcy.read("https://matriculaweb.unb.br/graduacao/oferta_dis.aspx?cod=" +
-                                           str(subjects_darcy_cods[aux]),
-                                           subjects_darcy_initials[aux],
-                                           subjects_darcy_cods[aux])
-    DataSinkSubject.write(subject)
+    for aux in range(size_subjects_darcy):
+
+        subject = DataSourceSubjectsDarcy.read("https://matriculaweb.unb.br/graduacao/oferta_dis.aspx?cod=" +
+                                               str(subjects_darcy_cods[aux]),
+                                               subjects_darcy_initials[aux],
+                                               subjects_darcy_cods[aux])
+        DataSinkSubject.write(subject)
+
+for aux in range(len(list_campi_numbers)):
+
+    request = requests.get("https://matriculaweb.unb.br/graduacao/curso_rel.aspx?cod=" +
+                                                 str(list_campi_numbers[aux]))
+    soup = BeautifulSoup(request.content, "html.parser")
+
+    courses = []
+    for tr in soup.find_all('tr'):
+
+        for td in tr.find_all('td'):
+            courses.append(td.text)
+
+    #DataSinkCampi.write(courses)
+    print(courses)
+#run()
